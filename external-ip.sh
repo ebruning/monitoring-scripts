31fa71ed1a6dacec584f2fbf30cc9d4d4c96dc31
Ethan Bruning <ebruning@gmail.com>
2016-08-10 13:38:13 -0700

    change name

--- external-ip.sh
+++ external-ip.sh
@@ -6,7 +6,7 @@ while :
 do
   result=$(wget http://ipinfo.io/ip -qO -)
   
-  curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "external,metric=address value=\"$result\""
+  curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "external_ip,metric=address value=\"$result\""
 
   sleep "$sleeptime"
 done
