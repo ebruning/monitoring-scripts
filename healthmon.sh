6b52e32676c12b5e64e12df4976962e6f5940be4
Ethan Bruning <ebruning@gmail.com>
2016-08-10 17:14:27 -0700

    update script remove fans

--- healthmon.sh
+++ healthmon.sh
@@ -10,6 +10,9 @@
 #you will never loose data by setting this to a larger value.
 sleeptime=30
 
+#servers=(172.16.1.231 172.16.1.232 172.16.1.233)
+servers=(172.16.1.231)
+
 #Command we will be using is ipmi tool - sudo apt-get install ipmitool
 
 #Sample Data
@@ -41,7 +44,8 @@ get_ipmi_data () {
     COUNTER=0
     while [  $COUNTER -lt 4 ]; do
         #Get ipmi data
-        ipmitool -H 172.16.1.233 -U ethan -P lighthouse sdr > tempdatafile
+        echo "processing $1"
+        ipmitool -H $1 -U ethan -P lighthouse sdr > tempdatafile
         cputemp=`cat tempdatafile | grep "CPU Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         systemtemp=`cat tempdatafile | grep "System Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         periphtemp=`cat tempdatafile | grep "Peripheral Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
@@ -51,13 +55,14 @@ get_ipmi_data () {
         dimma2temp=`cat tempdatafile | grep "DIMMA2 Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         dimmb1temp=`cat tempdatafile | grep "DIMMB1 Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         dimmb2temp=`cat tempdatafile | grep "DIMMB2 Temp" | cut -f2 -d"|" | grep -o '[0-9]\+'`
-        fan2=`cat tempdatafile | grep "FAN2" | cut -f2 -d"|" | grep -o '[0-9]\+'`
-        fan3=`cat tempdatafile | grep "FAN3" | cut -f2 -d"|" | grep -o '[0-9]\+'`
-        fan4=`cat tempdatafile | grep "FAN4" | cut -f2 -d"|" | grep -o '[0-9]\+'`
+        #fan2=`cat tempdatafile | grep "FAN2" | cut -f2 -d"|" | grep -o '[0-9]\+'`
+        #fan3=`cat tempdatafile | grep "FAN3" | cut -f2 -d"|" | grep -o '[0-9]\+'`
+        #fan4=`cat tempdatafile | grep "FAN4" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         fana=`cat tempdatafile | grep "FANA" | cut -f2 -d"|" | grep -o '[0-9]\+'`
         rm tempdatafile
         
-        if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fan2 -le 0 || $fan3 -le 0 || $fan4 -le 0 || $fana -le 0 ]]; 
+        #if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fan2 -le 0 || $fan3 -le 0 || $fan4 -le 0 || $fana -le 0 ]]; 
+        if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fana -le 0 ]]; 
         	then
                 echo "Retry getting data - received some invalid data from the read"
             else
@@ -78,27 +83,28 @@ print_data () {
     echo "DIMMA2 Temperature: $dimma2temp"
     echo "DIMMB1 Temperature: $dimmb1temp"
     echo "DIMMB2 Temperature: $dimmb2temp"
-    echo "Fan2 Speed: $fan2"
-    echo "Fan3 Speed: $fan3"
-    echo "Fan4 Speed: $fan4"
+#    echo "Fan2 Speed: $fan2"
+#    echo "Fan3 Speed: $fan3"
+#    echo "Fan4 Speed: $fan4"
     echo "FanA Speed: $fana"
 }
 
 write_data () {
     #Write the data to the database
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=cputemp value=$cputemp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=systemtemp value=$systemtemp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=periphtemp value=$periphtemp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=pchtemp value=$pchtemp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=vrmtemp value=$vrmtemp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=dimma1temp value=$dimma1temp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=dimma2temp value=$dimma2temp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=dimmb1temp value=$dimmb1temp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=dimmb2temp value=$dimmb2temp"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=fan2 value=$fan2"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=fan3 value=$fan3"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=fan4 value=$fan4"
-    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=proxmox,sensor=fana value=$fana"
+    echo "sending data for $1"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=cputemp value=$cputemp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=systemtemp value=$systemtemp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=periphtemp value=$periphtemp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=pchtemp value=$pchtemp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=vrmtemp value=$vrmtemp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=dimma1temp value=$dimma1temp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=dimma2temp value=$dimma2temp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=dimmb1temp value=$dimmb1temp"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=dimmb2temp value=$dimmb2temp"
+#    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=fan2 value=$fan2"
+#    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=fan3 value=$fan3"
+#    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=fan4 value=$fan4"
+    curl -i -XPOST 'http://localhost:8086/write?db=home' --data-binary "health_data,host=$1,sensor=fana value=$fana"
 }
 
 #Prepare to start the loop and warn the user
@@ -108,15 +114,18 @@ do
     #Sleep between readings
     sleep "$sleeptime"
     
-    get_ipmi_data
+    for server in "${servers[@]}"; do
+      get_ipmi_data $server
     
-    if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fan2 -le 0 || $fan3 -le 0 || $fan4 -le 0 || $fana -le 0 ]]; 
+    #if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fan2 -le 0 || $fan3 -le 0 || $fan4 -le 0 || $fana -le 0 ]]; 
+    if [[ $cputemp -le 0 || $systemtemp -le 0 || $periphtemp -le 0 || $pchtemp -le 0 || $vrmtemp -le 0 || $dimma1temp -le 0 || $dimma2temp -le 0 || $dimmb1temp -le 0 || $dimmb2temp -le 0 || $fana -le 0 ]]; 
     	then
             echo "Skip this datapoint - something went wrong with the read"
             
         else
             #Output console data for future reference
             print_data
-            write_data
+            write_data $server
     fi
+    done
 done
